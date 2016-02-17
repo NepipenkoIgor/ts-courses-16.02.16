5c90d21b0c72ff4b4d0afc9dd4dba15f45a9a608
Nikolay Kozhukharenko <nikolay.kozhuharenko@gmail.com>
2016-02-17 23:09:42 +0200

    building menu tree (half done)

--- lesson1/nikolay.kozhukharenko/app.ts
+++ lesson1/nikolay.kozhukharenko/app.ts
@@ -1,39 +1,48 @@
-type menuListItems = { title: string, items?: menuListItems | { title: string } }[]
+type menuListItems = { title: string, items?: menuListItems  }[]
 
 let firstMenu:menuListItems = [
     {
-        title: 'Животные', items: [
-        {
-            title: 'Млекопитающие', items: [
-            { title: 'Коровы' },
-            { title: 'Ослы' },
-            { title: 'Собаки' },
-            { title: 'Тигры' }
+        title: 'Животные',
+        items: [
+            {
+                title: 'Млекопитающие',
+                items: [
+                    { title: 'Коровы' },
+                    { title: 'Ослы' },
+                    { title: 'Собаки' },
+                    { title: 'Тигры' }
+                ]
+            },
+            {
+                title: 'Другие',
+                items: [
+                    { title: 'Змеи' },
+                    { title: 'Птицы' },
+                    { title: 'Ящерицы' },
+                ],
+            },
         ]
-        },
-        {
-            title: 'Другие', items: [
-            { title: 'Змеи' },
-            { title: 'Птицы' },
-            { title: 'Ящерицы' },
-        ],
-        },
-    ]
     },
     {
-        title: 'Рыбы', items: [
-        {
-            title: 'Аквариумные', items: [
-            { title: 'Гуппи' },
-            { title: 'Скалярии' }
+        title: 'Рыбы',
+        items: [
+            {
+                title: 'Аквариумные',
+                items: [
+                    { title: 'Гуппи' },
+                    { title: 'Скалярии' }
+                ]
+            },
+            {
+                title: 'Форель',
+                items: [
+                    { title: 'Морская форель' }
+                ]
+            },
         ]
-        },
-        {
-            title: 'Форель', items: [
-            { title: 'Морская форель' }
-        ]
-        },
-    ]
+    },
+    {
+        title: 'Звери'
     }
 ];
 
@@ -43,14 +52,27 @@ class ContextMenu {
     constructor(menuItems: menuListItems, requireMenu: HTMLElement) {
         this.menuItems = menuItems;
         this.requireMenu = requireMenu;
-        this.buildMenuTree(this.menuItems);
+        let menu = this.buildMenuTree(this.menuItems);
+        requireMenu.appendChild(menu);
+        console.log('menu', menu);
         this.addToggleToMenu();
     }
     private addToggleToMenu = ():void => {
         console.log('add toggle');
     };
-    private buildMenuTree = (menuItems: menuListItems):void => {
-        console.log('building menu. menuItems', menuItems);
+    private buildMenuTree = (menuItems: menuListItems):HTMLElement => {
+        let menuContainer = document.createElement('ul');
+        let menuItemContainer:HTMLElement;
+        for (let item of menuItems) {
+            menuItemContainer =  document.createElement('li');
+            if (item.hasOwnProperty('items')) {
+                menuItemContainer.appendChild(this.buildMenuTree(item.items));
+            } else {
+                menuItemContainer.innerHTML = `<li><a>${item.title}</a></li>`;
+            }
+            menuContainer.appendChild(menuItemContainer);
+        }
+        return menuContainer;
     };
 }
 
